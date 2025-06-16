99c982a990a7d614aa88f2493a0e104ca2bbc180
unknown899 <kaiwy58.ee11@nycu.edu.tw>
2025-06-16 20:42:44 +0800

    modify backgriund

--- level_folder/level_2/config.py
+++ level_folder/level_2/config.py
@@ -37,7 +37,7 @@ level_config = {
     ],
     "spawn_interval": 2500,
     "survival_time": 90,
-    "background_path": "background/background2.png",
+    "background_path": "background/background4.png",
     "our_tower": {
         "y": 140,
         "width": 350,
